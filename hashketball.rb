90fe6528bba0e40d93d1b1326e163963bdb52430
Shemar Dacosta <dshemar6@gmail.com>
2019-01-15 15:25:47 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -123,7 +123,7 @@ def num_points_scored(player)
   
   game_hash.map do |key, value|
     binding.pry
-    value[:players].select do |key, value|
+    value[:players].find do |key, value|
       if key == player
         return value[:points]
       end
